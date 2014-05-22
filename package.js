db41b0018f06e70a65300c807c5a5dd636eaef95
Giuseppe <giuse88@gmail.com>
2014-05-22 08:28:25 +0100

    Added function to load static files.

--- package.js
+++ package.js
@@ -3,7 +3,50 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-	api.use('', 'client');
-	/* TODO */
-	api.add_files(['TODO' ], 'client');
+	var clientFiles=getFilesFromFolder("my-package","client");
+	console.log(clientFiles);
+	api.add_files(clientFiles,"client");
 });
+
+
+	/******************************
+	*				Helper methods        *
+	*******************************/
+
+function getFilesFromFolder(packageName,folder){
+	// local imports
+	//var _=Npm.require("underscore");
+	var fs=Npm.require("fs");
+	var path=Npm.require("path");
+	// helper function, walks recursively inside nested folders and return absolute filenames
+	function walk(folder){
+		var filenames=[];
+		// get relative filenames from folder
+		var folderContent=fs.readdirSync(folder);
+		// iterate over the folder content to handle nested folders
+		_.each(folderContent,function(filename){
+			// build absolute filename
+			var absoluteFilename=folder+path.sep+filename;
+			// get file stats
+			var stat=fs.statSync(absoluteFilename);
+			if(stat.isDirectory()){
+				// directory case => add filenames fetched from recursive call
+				filenames=filenames.concat(walk(absoluteFilename));
+			}
+			else{
+				// file case => simply add it
+				filenames.push(absoluteFilename);
+			}
+		});
+		return filenames;
+	}
+	// save current working directory (something like "/home/user/projects/my-project")
+	var cwd=process.cwd();
+	// chdir to our package directory
+	process.chdir("packages"+path.sep+packageName);
+	// launch initial walk
+	var result=walk(folder);
+	// restore previous cwd
+	process.chdir(cwd);
+	return result;
+}
