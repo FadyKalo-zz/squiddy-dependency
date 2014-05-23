4ae3ddbc133d6e00bee35898562ba1b5018b3259
Giuseppe <giuse88@gmail.com>
2014-05-23 17:07:39 +0100

    Added fonts.

--- package.js
+++ package.js
@@ -4,10 +4,6 @@ Package.describe({
 
 Package.on_use(function (api, where) {
 
-  /*
-   var clientFiles = getFilesFromFolder("squiddy-dependency" , "public")
-   console.log(clientFiles);
-   */
   var clientFiles = [
 
     /* ========= CSS ========= */
@@ -31,50 +27,23 @@ Package.on_use(function (api, where) {
     "public/assets/plugins/jquery-lazyload/jquery.lazyload.min.js",
     "public/assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js",
     /* =========    END     ========= */
+
+    /* =========    IMG     ========= */
+    "public/assets/img/work.jpg",
+    /* =========    END     ========= */
+
+    /* =========    FONT    ========= */
+    "public/assets/img/work.jpg",
+    "public/assets/plugins/font-awesome/fonts/fontawesome-webfont.woff",
+    "public/assets/plugins/font-awesome/fonts/fontawesome-webfont.ttf",
+    "public/assets/plugins/font-awesome/fonts/fontawesome-webfont.svg"
+    /* =========    END     ========= */
   ];
+  //
   api.add_files(clientFiles, "client");
+  //
+  console.log("SQUIDDY-DEPENDENCY : Loaded asset.")
 });
 
 
-/******************************
- *        Helper methods        *
- *******************************/
 
-function getFilesFromFolder(packageName, folder) {
-  // local imports
-  var _ = Npm.require("underscore");
-  var fs = Npm.require("fs");
-  var path = Npm.require("path");
-  // helper function, walks recursively inside nested folders and return absolute filenames
-  function walk(folder) {
-    var filenames = [];
-    // get relative filenames from folder
-    var folderContent = fs.readdirSync(folder);
-    // iterate over the folder content to handle nested folders
-    _.each(folderContent, function (filename) {
-      // build absolute filename
-      var absoluteFilename = folder + path.sep + filename;
-      // get file stats
-      var stat = fs.statSync(absoluteFilename);
-      if (stat.isDirectory()) {
-        // directory case => add filenames fetched from recursive call
-        filenames = filenames.concat(walk(absoluteFilename));
-      }
-      else {
-        // file case => simply add it
-        filenames.push(absoluteFilename);
-      }
-    });
-    return filenames;
-  }
-
-  // save current working directory (something like "/home/user/projects/my-project")
-  var cwd = process.cwd();
-  // chdir to our package directory
-  process.chdir("packages" + path.sep + packageName);
-  // launch initial walk
-  var result = walk(folder);
-  // restore previous cwd
-  process.chdir(cwd);
-  return result;
-}
